f9c1f58bba1ee4fbd64b3a6c563db10266c01949
AgusIglesias72 <enpalabrass@gmail.com>
2023-11-21 15:11:24 -0300

    add new filter

--- src/tiendanube/index.js
+++ src/tiendanube/index.js
@@ -33,7 +33,7 @@ const ORDER_STATUS = {
   cancelled: 'Cancelada',
 }
 
-export const createOrder = async (id) => {
+export const createOrder = async (id, event) => {
   const existingIds = await checkIds()
 
   if (existingIds.includes(String(id))) {
@@ -84,110 +84,12 @@ export const createOrder = async (id) => {
       }
     }
 
-    if (payment_status === 'paid') {
+    if (payment_status === 'paid' && event === 'order/created') {
       return {
         status: 'PAID',
         message:
           'Pagado: Se creará la venta en el sistema con la notificación de pago',
       }
-    }
-  }
-
-  const {
-    number,
-    created_at,
-    status,
-    paid_at,
-    shipping_status,
-    shipped_at,
-    contact_name,
-    contact_email,
-    contact_phone,
-    contact_identification,
-    shipping_option,
-    products,
-    subtotal,
-    discount,
-    total,
-    currency,
-  } = data
-
-  const csvLines = []
-
-  products.map((product) => {
-    const valuesToInclude = [
-      [
-        number,
-        new Date(created_at).toLocaleString('es-AR'),
-        ORDER_STATUS[status],
-        PAYMENT_STATUS[payment_status],
-        new Date(paid_at).toLocaleString('es-AR'),
-        contact_name,
-        contact_email,
-        contact_identification,
-        contact_phone,
-        shipping_status,
-        null,
-        shipping_option,
-        product.name,
-        product.quantity,
-        gateway_name,
-        subtotal,
-        discount,
-        total,
-        currency,
-        id,
-      ],
-    ]
-
-    csvLines.push(...valuesToInclude)
-  })
-
-  await appendData('Ventas!R2', csvLines)
-
-  return {
-    status: 'OK',
-    message: 'Nueva Venta con retiro por local',
-    data: {
-      number,
-      contact_name,
-      contact_email,
-      contact_phone,
-    },
-  }
-}
-
-export const createPaidOrder = async (id) => {
-  const existingIds = await checkIds()
-
-  if (existingIds.includes(String(id))) {
-    return {
-      status: 'ALREADY_EXISTS',
-      message: 'Ya existe la venta',
-    }
-  }
-
-  const URL = `https://api.tiendanube.com/v1/1705915/orders/${id}`
-
-  const headers = {
-    'Content-Type': 'application/json',
-    Authentication: AUTH_TIENDANUBE,
-    'User-Agent': 'En Palabras (enpalabrass@gmail.com)',
-  }
-
-  const response = await fetch(URL, {
-    method: 'GET',
-    headers,
-  })
-
-  const data = await response.json()
-
-  const { shipping_option_code, gateway_name, payment_status, gateway } = data
-
-  if (shipping_option_code !== 'table_6629488') {
-    return {
-      status: 'NOT_VALID_SHIPPING_OPTION',
-      message: 'No es una venta con retiro por local',
     }
   }
 
